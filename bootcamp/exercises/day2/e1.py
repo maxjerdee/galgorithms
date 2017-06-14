a812fec415886aab811c3f4550647ce4951d9fb2
maxjerdee <mjerdee@princeton.edu>
2017-06-14 17:48:23 -0400

    Done w day1

--- bootcamp/exercises/day2/e1.py
+++ bootcamp/exercises/day2/e1.py
@@ -1,5 +1,4 @@
 import numpy as np
-import estimate_pi as ep
 a1 = np.ones((4,4), dtype=np.int16)
 a1[2][3] = 2
 a1[3][1] = 6
@@ -8,5 +7,3 @@ a2 = np.zeros((5,5))
 for i in range(6):
 	a2[i - 1][i - 1] = i
 print(a2)
-
-ep.go(10)
--- bootcamp/exercises/day2/e4.py
+++ bootcamp/exercises/day2/e4.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def gaussian2D(x,y,mean_xy,cov):
-	return np.exp(-1/2*np.dot(np.dot(x - mean_xy, cov), y - mean_xy))
+	tx = np.stack([x,y], axis=1);
+	return np.array([np.dot(np.dot((tx[i] - mean_xy),cov),np.transpose(tx[i] - mean_xy)) for i in range(10000)]);
+	#return np.exp(-1/2*np.dot(np.dot(np.transpose(tx - mean_xy),cov),(tx - mean_xy)));
 
 x = np.arange(0.,10.,0.1)
 y  = np.arange(0.,10.,0.1)
 x,y = np.meshgrid(x,y)
-mean_xy = np.array([5.,4.])
-cov = np.array([[1.,1.],[1.,2.]])**2.
+mean_xy = np.array([5.,5.])
+cov = np.array([[1,1],[0.,1]])**2.
+f = gaussian2D(x.ravel(), y.ravel(), mean_xy, cov)
+f.reshape(100,100)
 
-print(x - mean_xy[0])
-print(y)
-#f = gaussian2D(x.ravel(), y.ravel(), mean_xy, cov)
-#f.reshape(100,100)
-
-#plt.imshow(f.reshape(100,100))
+plt.imshow(f.reshape(100,100))
+plt.show()
