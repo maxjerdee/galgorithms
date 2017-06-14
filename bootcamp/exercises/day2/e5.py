09a03baa507953874231c30e66149bdb2545ef0a
Max Jerdee <mjerdee@gunn.princeton.edu>
2017-06-14 15:17:34 -0400

    P

--- bootcamp/exercises/day2/e5.py
+++ bootcamp/exercises/day2/e5.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def sim(n):
-	darts = np.array([np.random.rand(2)*2 - 1 for i in range(n)])
-	plt.plot(darts)
-sim(10)
+    darts = np.array([np.random.rand(2)*2 - 1 for i in range(n)]);
+    within = np.array([(darts[i][0]**2 + darts[i][1]**2) < 1 for i in range(n)]).sum();
+    plt.plot(darts);
+    plt.show();
+    print(float(within*4)/n);
+sim(100000)
